84d308bd29f6a45a506402263fcb69db7af17afe
sidinyjunior <sidinyjunior@gmail.com>
2026-01-04 00:17:59 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'projeto-app-biblia-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/assets/icone_leao_biblia_app.png',
+  '/acf.json',
+  '/nvi.json',
+  '/kja.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
