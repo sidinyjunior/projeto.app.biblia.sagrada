a9d1b3c0ca06adc9f299f31136113cceb87e57e7
sidinyjunior <sidinyjunior@gmail.com>
2026-01-04 17:51:25 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'projeto-app-biblia-v8'; // Mude para v4, v5 sempre que atualizar
+const CACHE_NAME = 'projeto-app-biblia-v9'; // Mude para v4, v5 sempre que atualizar
 const ASSETS = [
   './',
   './index.html',
